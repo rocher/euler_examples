0da45265fac555e4e3ea917a9487b9c3c875b506
Francesc Rocher <francesc.rocher@gmail.com>
2023-01-14 17:55:32 +0100

    Typo

--- src/p0001_multiples_of_3_or_5.adb
+++ src/p0001_multiples_of_3_or_5.adb
@@ -4,7 +4,7 @@
 --  SPDX-License-Identifier: CC-BY-NC-SA-4.0
 --  https://creativecommons.org/licenses/by-nc-sa/4.0/
 --
--------------------------------------------------------------------------------
+--  ---------------------------------------------------------------------------
 --
 --  The following problem is taken from Project Euler:
 --
