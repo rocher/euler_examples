aeb0fd90441656973d8d4eaa4ac325da64a2d810
Francesc Rocher <francesc.rocher@gmail.com>
2023-03-04 13:02:51 +0100

    Format comments

--- src/tui/0001-0050/t0015_lattice_paths.adb
+++ src/tui/0001-0050/t0015_lattice_paths.adb
@@ -5,23 +5,6 @@
 --  https://creativecommons.org/licenses/by-nc-sa/4.0/
 --
 --  ---------------------------------------------------------------------------
---
---  The following problem is taken from Project Euler:
---
---                 https://projecteuler.net/problem=15
---
---  Lattice paths
---  -------------
---
---  Starting in the top left corner of a 2×2 grid, and only being able to
---  move to the right and down, there are exactly 6 routes to the bottom
---  right corner.
---
---                 [ see diagram ]
---
---  How many such routes are there through a 20×20 grid?
---
--------------------------------------------------------------------------------
 
 with P0015_Lattice_Paths;
 with TUI_Runner;
